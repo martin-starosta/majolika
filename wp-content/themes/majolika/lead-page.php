eb09929754bb2b1f0d4f69b94b0c9cc2e907d79a
Martin Starosta <martin@mayorsoft.eu>
2016-12-01 17:10:39 +0100

    Zmena uvodnej stranky (obrazok a text)

--- wp-content/themes/majolika/lead-page.php
+++ wp-content/themes/majolika/lead-page.php
@@ -10,6 +10,9 @@ get_header(); ?>
   <div class="container">
     <p class="subheader white-text">Sme súčasťou stáročnej tradície</p>
   	<p class="header white-text">Tvoríme dušu regiónu</p>
+
+    <p class="white-text" style="margin-top: 3em;">Stránku pre vás pripravujeme.</p>
+    <p class="white-text">Pre bližšie informácie kontaktujte: <a class="white-text" href="mailto:info@majolikamodra.sk">info@majolikamodra.sk</a></p>
   </div>
 </div>
 
